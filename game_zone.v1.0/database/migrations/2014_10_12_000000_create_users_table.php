50304f1f16e4b93a7443edd981cafa8c81ac4c4c
mwinnona <winnonamaye@hotmail.com>
2020-05-11 20:11:34 -0500

    Migracion user

--- game_zone.v1.0/database/migrations/2014_10_12_000000_create_users_table.php
+++ game_zone.v1.0/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name',115);
+            $table->string('lastname',115);
             $table->string('email')->unique();
+            $table->boolean('status')->default(1);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo',115);
+            $table->string('token_user',115);
             $table->rememberToken();
             $table->timestamps();
         });
