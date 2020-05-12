f83efd0e9fb7d55849e46f6828431a2d08d8bf28
VectorXIII <vector517667@hotmail.com>
2020-05-11 22:01:30 -0500

    nel perro

---
+++ game_zone.v1.0/database/migrations/2020_05_12_015207_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 115)->nullable();
+            $table->string('description', 400)->nullable();
+            $table->integer('type')->nullable();
+            $table->string('plataform')->nullable();
+            $table->string('category')->nullable();
+            $table->float('price')->nullable();
+            $table->string('image', 115)->nullable();
+            $table->boolean('status')->default(1);
+            $table->string('token_product',115)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
