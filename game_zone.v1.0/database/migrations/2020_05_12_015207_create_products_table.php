8d973003c764bf016c9dfb47ca12217df4fe4f65
VectorXIII <vector517667@hotmail.com>
2020-05-12 14:19:35 -0500

    La base de datos a sido actualizada

--- game_zone.v1.0/database/migrations/2020_05_12_015207_create_products_table.php
+++ game_zone.v1.0/database/migrations/2020_05_12_015207_create_products_table.php
@@ -17,13 +17,14 @@ class CreateProductsTable extends Migration
             $table->id();
             $table->string('name', 115)->nullable();
             $table->string('description', 400)->nullable();
-            $table->integer('type')->nullable();
+            $table->integer('type_product')->nullable();
             $table->string('plataform')->nullable();
-            $table->string('category')->nullable();
+            $table->string('gender')->nullable();
             $table->float('price')->nullable();
             $table->string('image', 115)->nullable();
             $table->date('release_date')->nullable();
             $table->boolean('status')->default(1);
+            $table->integer('stock')->nullable();
             $table->string('token_product',115)->nullable();
             $table->timestamps();
         });
