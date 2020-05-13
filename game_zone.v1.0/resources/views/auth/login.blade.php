a406d63927f6ed9f83ccb34c2c9385e9d8d52da0
VectorXIII <vector517667@hotmail.com>
2020-05-12 22:43:26 -0500

    actualizado logo

--- game_zone.v1.0/resources/views/auth/login.blade.php
+++ game_zone.v1.0/resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -78,7 +78,7 @@
                                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
             
                                                 <label class="form-check-label" for="remember">
-                                                    {{ __('Remember Me') }}
+                                                    {{ __('...       Recordar') }}
                                                 </label>
                                             </div>
                                         </div>
