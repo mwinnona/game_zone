73f52764b2bf0549bee99149b96a61fec4f4024f
mwinnona <winnonamaye@hotmail.com>
2020-06-08 18:45:45 -0500

    Prueba

--- game_zone.v1.0/public/js/products.js
+++ game_zone.v1.0/public/js/products.js
@@ -1,5 +1,47 @@
 $(document).ready(function(){
     c1=0;
+    c2=0;
+    c3=0;
+
+    t1=0;
+    t2=0;
+
+    p1=0;
+    p2=0;
+    p3=0;
+    p4=0;
+
+    g1=0;
+    g2=0;
+    g3=0;
+    g4=0;
+    g5=0;
+    g6=0;
+    g7=0;
+    g8=0;
+    g9=0;
+    $('#category-1').val(0);
+    $('#category-2').val(0);
+    $('#category-3').val(0);
+
+    $('#type-1').val(0);
+    $('#type-2').val(0);
+
+    $('#price-1').val(0);
+    $('#price-2').val(0);
+    $('#price-3').val(0);
+    $('#price-4').val(0);
+
+    $('#brand-1').val(0);
+    $('#brand-2').val(0);
+    $('#brand-3').val(0);
+    $('#brand-4').val(0);
+    $('#brand-5').val(0);
+    $('#brand-6').val(0);
+    $('#brand-7').val(0);
+    $('#brand-8').val(0);
+    $('#brand-9').val(0);
+
     producto();
  });
 
@@ -13,16 +55,185 @@ $(document).ready(function(){
     }
   });
 
- $(document).on('change','input[type="checkbox"]' ,function(e) {
-    if(this.id=="fiscal") {
-        if(this.checked) $('#id_fiscal').val(this.value);
-        else $('#id_fiscal').val("");
+  $( "#category-2" ).change(function() {
+    if(c2==0){
+        $('#category-2').val(1);
+        c2=1;
+    }else{
+        $('#category-2').val(0);
+        c2=0;
     }
-    if(this.id=="nacional") {
-        if(this.checked) $('#id_nacional').val(this.value);
-        else $('#id_nacional').val("");
+  });
+
+  $( "#category-3" ).change(function() {
+    if(c3==0){
+        $('#category-3').val(1);
+        c3=1;
+    }else{
+        $('#category-3').val(0);
+        c3=0;
     }
-});
+  });
+
+  $( "#type-1" ).change(function() {
+    if(t1==0){
+        $('#type-1').val(1);
+        t1=1;
+    }else{
+        $('#type-1').val(0);
+        t1=0;
+    }
+  });
+
+  $( "#type-2" ).change(function() {
+    if(t1==0){
+        $('#type-2').val(1);
+        t1=1;
+    }else{
+        $('#type-2').val(0);
+        t1=0;
+    }
+  });
+
+  
+
+  $( "#price-1" ).change(function() {
+    if(p1==0){
+        $('#price-1').val(1);
+        p1=1;
+    }else{
+        $('#price-1').val(0);
+        p1=0;
+    }
+  });
+
+  $( "#price-2" ).change(function() {
+    if(p1==0){
+        $('#price-2').val(1);
+        p2=1;
+    }else{
+        $('#price-2').val(0);
+        p2=0;
+    }
+  });
+
+  $( "#price-3" ).change(function() {
+    if(p3==0){
+        $('#price-3').val(1);
+        p3=1;
+    }else{
+        $('#price-3').val(0);
+        p3=0;
+    }
+  });
+
+  $( "#price-4" ).change(function() {
+    if(p4==0){
+        $('#price-4').val(1);
+        p4=1;
+    }else{
+        $('#price-4').val(0);
+        p4=0;
+    }
+  });
+
+  $( "#brand-1" ).change(function() {
+    if(g1==0){
+        $('#brand-1').val(1);
+        g1=1;
+    }else{
+        $('#brand-1').val(0);
+        g1=0;
+    }
+  });
+
+  $( "#brand-2" ).change(function() {
+    if(g2==0){
+        $('#brand-2').val(1);
+        g2=1;
+    }else{
+        $('#brand-2').val(0);
+        g2=0;
+    }
+  });
+
+  $( "#brand-3" ).change(function() {
+    if(g3==0){
+        $('#brand-3').val(1);
+        g3=1;
+    }else{
+        $('#brand-3').val(0);
+        g3=0;
+    }
+  });
+
+  $( "#brand-4" ).change(function() {
+    if(g4==0){
+        $('#brand-4').val(1);
+        g4=1;
+    }else{
+        $('#brand-4').val(0);
+        g4=0;
+    }
+  });
+
+  $( "#brand-5" ).change(function() {
+    if(g5==0){
+        $('#brand-5').val(1);
+        g5=1;
+    }else{
+        $('#brand-5').val(0);
+        g5=0;
+    }
+  });
+
+  $( "#brand-6" ).change(function() {
+    if(g6==0){
+        $('#brand-6').val(1);
+        g6=1;
+    }else{
+        $('#brand-6').val(0);
+        g6=0;
+    }
+  });
+
+  $( "#brand-7" ).change(function() {
+    if(g7==0){
+        $('#brand-7').val(1);
+        g7=1;
+    }else{
+        $('#brand-7').val(0);
+        g7=0;
+    }
+  });
+
+
+  $( "#brand-8" ).change(function() {
+    if(g8==0){
+        $('#brand-8').val(1);
+        g8=1;
+    }else{
+        $('#brand-8').val(0);
+        g8=0;
+    }
+  });
+
+  $( "#brand-9" ).change(function() {
+    if(g9==0){
+        $('#brand-9').val(1);
+        g9=1;
+    }else{
+        $('#brand-9').val(0);
+        g9=0;
+    }
+  });
+
+  
+
+
+
+
+ 
 
  function producto(){
        
