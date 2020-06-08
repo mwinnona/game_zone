618dfa9a050a2a73e8cd32cf7839627f16832b48
mwinnona <winnonamaye@hotmail.com>
2020-06-08 17:52:09 -0500

    Checkbox

--- game_zone.v1.0/public/js/products.js
+++ game_zone.v1.0/public/js/products.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
-    
+    c1=0;
     producto();
  });
 
+ $( "#category-1" ).change(function() {
+    if(c1==0){
+        $('#category-1').val(1);
+        c1=1;
+    }else{
+        $('#category-1').val(0);
+        c1=0;
+    }
+  });
+
+ $(document).on('change','input[type="checkbox"]' ,function(e) {
+    if(this.id=="fiscal") {
+        if(this.checked) $('#id_fiscal').val(this.value);
+        else $('#id_fiscal').val("");
+    }
+    if(this.id=="nacional") {
+        if(this.checked) $('#id_nacional').val(this.value);
+        else $('#id_nacional').val("");
+    }
+});
 
  function producto(){
        
