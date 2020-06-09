249cd1af5bf1a918fe1371ef35945c71af6e8701
mwinnona <winnonamaye@hotmail.com>
2020-06-09 15:18:49 -0500

    n

--- game_zone.v1.0/resources/views/layouts/mainlayout.blade.php
+++ game_zone.v1.0/resources/views/layouts/mainlayout.blade.php
@@ -307,9 +307,9 @@
 	</body>
 </html>
 <!-- jQuery Plugins -->
+<script src={{ asset("js/jquery-3.4.1.min.js")}}></script>
 <script src={{ asset("js/jquery.min.js")}}></script>
 <script src={{ asset("js/bootstrap.min.js")}}></script>
-<script src={{ asset("js/jquery-3.4.1.min.js")}}></script>
 <script src={{ asset("js/slick.min.js")}}></script>
 <script src={{ asset("js/nouislider.min.js")}}></script>
 <script src={{ asset("js/jquery.zoom.min.js")}}></script>
